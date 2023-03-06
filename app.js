5da36b64b17fee361c38e41c246d6892b767766a
sihar <luq5nvd@gmail.com>
2023-03-06 20:07:25 +0700

    modify connectDatabase, insert, find documents

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ const assert = require('assert')
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "mongodb://127.0.0.1:27017";
 
-const dbName = 'fruitsDB'
+
+
+
 
 // Connection URI
 // Create a new MongoClient
 const client = new MongoClient(uri);
-async function run() {
+
+const database = client.db('fruitsDB')
+const fruits = database.collection("fruits")
+
+async function connectDatabase() {
   try {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Establish and verify connection
-    await client.db(dbName).command({ ping: 1 });
+    await client.db(database).command({ ping: 1 });
     console.log("Connected successfully to server");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -22,4 +28,46 @@ async function run() {
   }
 }
 
-run().catch(console.dir);
+async function insertDocuments() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    
+    const docs = [
+      {
+        name  : "apple",
+        score: 8,
+        review:"great fruit"
+      },
+      {
+        name  : "orange",
+        score: 6,
+        review:"kinda sour"
+      },
+      {
+        name  : "banana",
+        score: 9,
+        review:"great stuff"
+      }
+    ]
+
+    const options = { ordered: true }
+    
+    const result = await fruits.insertMany(docs, options)
+    console.log(`${result.insertedCount} documents were inserted`);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function findAllDocuments() {
+  try {
+    const cursor = fruits.find({})
+    const allValues = await cursor.toArray()
+    console.log(allValues);
+  } finally {
+    await client.close();
+  }
+}
+
+findAllDocuments().catch(console.dir);
